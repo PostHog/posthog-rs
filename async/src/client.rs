92e3957b95cc9dc7222146299cc41bcd804c36b0
Nathan Flurry <developer@nathanflurry.com>
2023-01-13 05:00:56 +0000

    Make async backend handle non-200 errors correctly

--- async/src/client.rs
+++ async/src/client.rs
@@ -1,6 +1,6 @@
 use posthog_core::event::{Event, InnerEvent, InnerEventBatch};
-use reqwest::header::CONTENT_TYPE;
-use reqwest::Client as HttpClient;
+use reqwest::{Client as HttpClient, Method};
+use serde::{de::DeserializeOwned, Serialize};
 
 use crate::client_options::ClientOptions;
 use crate::error::Error;
@@ -22,29 +22,41 @@ impl Client {
         }
     }
 
-    pub async fn capture(&self, event: Event) -> Result<(), Error> {
-        let inner_event = InnerEvent::new(event, self.options.api_key.clone());
-        let _res = self
+    async fn send_request<P: AsRef<str>, Body: Serialize, Res: DeserializeOwned>(
+        &self,
+        method: Method,
+        path: P,
+        body: &Body,
+    ) -> Result<Res, Error> {
+        let res = self
             .http_client
-            .post(self.options.api_endpoint.clone())
-            .header(CONTENT_TYPE, "application/json")
-            .body(serde_json::to_string(&inner_event).expect("unwrap here is safe"))
+            .request(
+                method,
+                format!("{}{}", self.options.api_endpoint, path.as_ref()),
+            )
+            .json(body)
             .send()
             .await
-            .map_err(|source| Error::Connection { source })?;
+            .map_err(|source| Error::SendRequest { source })?
+            .error_for_status()
+            .map_err(|source| Error::ResponseStatus { source })?
+            .json::<Res>()
+            .await
+            .map_err(|source| Error::DecodeResponse { source })?;
+        Ok(res)
+    }
+
+    pub async fn capture(&self, event: Event) -> Result<(), Error> {
+        let inner_event = InnerEvent::new(event, self.options.api_key.clone());
+        self.send_request::<_, _, serde_json::Value>(Method::POST, "/capture/", &inner_event)
+            .await?;
         Ok(())
     }
 
     pub async fn capture_batch(&self, events: Vec<Event>) -> Result<(), Error> {
-        let inner_event = InnerEventBatch::new(events, self.options.api_key.clone());
-        let _res = self
-            .http_client
-            .post(self.options.api_endpoint.clone())
-            .header(CONTENT_TYPE, "application/json")
-            .body(serde_json::to_string(&inner_event).expect("unwrap here is safe"))
-            .send()
-            .await
-            .map_err(|source| Error::Connection { source })?;
+        let inner_event_batch = InnerEventBatch::new(events, self.options.api_key.clone());
+        self.send_request::<_, _, serde_json::Value>(Method::POST, "/batch/", &inner_event_batch)
+            .await?;
         Ok(())
     }
 }
