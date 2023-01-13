e2667783f49d14e2cf74f49ca19e2b6663f02263
Nathan Flurry <developer@nathanflurry.com>
2023-01-13 05:06:10 +0000

    Update test event names to specify which test is running

--- async/tests/basic.rs
+++ async/tests/basic.rs
@@ -12,7 +12,7 @@ async fn capture() {
     let mut child_map = HashMap::new();
     child_map.insert("child_key1", "child_value1");
 
-    let mut event = Event::new("test", "1234");
+    let mut event = Event::new("test async capture", "1234");
     event.insert_prop("key1", "value1").unwrap();
     event.insert_prop("key2", vec!["a", "b"]).unwrap();
     event.insert_prop("key3", child_map).unwrap();
@@ -29,7 +29,7 @@ async fn capture_batch() {
             let mut child_map = HashMap::new();
             child_map.insert("child_key1", "child_value1");
 
-            let mut event = Event::new("test", "1234");
+            let mut event = Event::new("test async capture batch", "1234");
             event.insert_prop("key1", "value1").unwrap();
             event.insert_prop("key2", vec!["a", "b"]).unwrap();
             event.insert_prop("key3", child_map).unwrap();
