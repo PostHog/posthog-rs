b11f877209a60bf334bc863636a57cd9b639f44c
lmtr0 <lmtr0@proton.me>
2025-02-21 11:53:35 -0300

    feat(api): Add OpenAPI-based API generation

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -5,3 +5,9 @@ pub mod query;
 pub use client::PostHogAPIClient;
 pub use error::PostHogAPIError;
 pub use query::{QueryRequest, QueryResponse};
+
+mod openapi {
+    use progenitor::generate_api;
+
+    generate_api!("./schema.yaml");
+}
