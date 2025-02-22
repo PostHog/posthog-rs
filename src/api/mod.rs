0b8cebf4d3fbae98efbde3d5a7beb68019305afc
lmtr0 <lmtr0@proton.me>
2025-02-22 14:51:20 -0300

    feat(schema): update property filters and query responses

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -11,3 +11,16 @@ mod openapi {
 
     generate_api!("./schema.yaml");
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_client() {
+        let client = openapi::Client::new("hello world");
+        // client.environments_app_metrics_retrieve(project_id, id)
+        // assert!(client.is_ok());
+    }
+}
